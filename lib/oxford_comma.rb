45b63c097e4d5fa8c3195dfc582fbd31b33c3795
Stuart Hahn <stuart.a.hahn@gmail.com>
2019-03-03 21:42:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
     array.join()
   elsif array.size == 2
     array.join(" and ")
-  elsif array.size == 3
-    array.join(" and ")
+  elsif array.size >= 3
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
   end
 end
